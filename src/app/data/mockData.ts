82d2b36385e1fe00c96a2adfe72a1be887cbb469
Caio Batista <caio_b_s@hotmail.com>
2023-08-18 16:28:39 -0300

    fix: content page is now fetching data from mockdata properly

--- src/app/data/mockData.ts
+++ src/app/data/mockData.ts
@@ -3,24 +3,24 @@ export const data = [
         "id": 1,
         "photo": '../../../assets/img/stock01.jpg',
         "title": 'Route 66: A legendary road',
-        "text": 'The road is full of things that hark back to the past too from old cars dotted along the road to old signs and historic buildings. For anyone who loves history or just wants a bit of an old fashioned vacation Route 66 fits the bill.'
+        "text": 'The creation, popularity and demise of Route 66 has been set around some major events in US history and as such its actually a really important historic monument. This is why its being protected and revived in many places along the route. Route 66 began when paved roads werent even a thing in the west of the US and its grown up around the growth of the car as a way of life. It was there as families wanted to have a vacation and explore more of the country. It saw the migration of many people after the depression and the dust bowl catastrophe in Oklahoma. Some went westward to California in search of a better life (as told in the book The Grapes of Wrath) and some went north to Chicago and the industrial cities of the mid west. Route 66 tells the story of westward expansion in more modern times but also of the endless desire to move forward with technology. Route 66 overtook the railroads which were the peak of transport at their time and then saw as Interstates and other highways overtook it afterwards. Towns were bypassed, with some turning to ghost towns, as cars and trucks improved and didnt need the endless stops any more.'
     },
     {
         "id": 2,
         "photo": '../../../assets/img/stock02.jpg',
         "title": 'Astronomic picture wins the Pulitzer',
-        "text": 'Picture from the Tulu Mountains with the Milky Way in the background wins Pulitzer.'
+        "text": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel hendrerit felis. Ut pellentesque finibus pharetra. Sed eu quam sit amet magna tempor imperdiet sed vel tellus. Ut elit augue, mattis in odio et, luctus tincidunt purus. Aenean consequat sed mi molestie ornare. Nam in blandit orci. In hac habitasse platea dictumst. Praesent sed dignissim augue. Suspendisse eget orci feugiat, ornare nisi vel, scelerisque sapien. Cras cursus, dolor eget volutpat venenatis, metus odio fringilla metus, id accumsan dolor metus eget lectus. Donec lectus sapien, condimentum sit amet commodo auctor, consequat quis urna. Phasellus non purus ipsum. Nam faucibus at nulla vel semper. Suspendisse at lacinia ligula. Donec quis porttitor est. Duis vulputate fringilla viverra. In ultricies ultricies erat, eget mollis leo mattis non. Aliquam at consequat justo. Morbi neque quam, consectetur eget convallis ut, euismod non libero. Pellentesque vulputate nisl non volutpat efficitur. Praesent purus magna, vulputate et vulputate ut, vehicula eu augue. Quisque non vehicula nibh, id vulputate nunc. Nulla mattis sem ac viverra gravida. In hac habitasse platea dictumst. Nulla nec metus rutrum, fermentum felis vel, dapibus orci. Curabitur augue sem, tristique ut nisl id, interdum mollis nulla. In vitae massa quam. Nunc tempor elit in erat mattis volutpat. Donec vitae sapien facilisis turpis ultrices convallis. Ut ac porta quam. In ullamcorper libero ac enim vestibulum facilisis. Vestibulum vestibulum est pharetra, mollis nisi vel, gravida dolor. Praesent mollis, urna eget aliquet commodo, arcu enim hendrerit libero, id luctus lorem ligula eu lacus. Donec eu lacinia felis.'
     },
     {
         "id": 3,
         "photo": '../../../assets/img/stock03.webp',
         "title": 'Get rid of stress in your life',
-        "text": 'Is stress making you angry and grouchy? Stress relievers can help bring back calm and peace to your life.'
+        "text": 'You might want to do it all, but you cant at least not without paying a price. Learning to say no or being willing to delegate can help you manage your to-do list and your stress. Healthy boundaries are important in a wellness journey. Everyone has physical and emotional limits. Saying yes may seem like an easy way to keep the peace, prevent conflicts and get the job done right. But instead, it may cause you inner conflict because your needs and those of your family come second. Putting yourself second can lead to stress, anger, resentment and even the wish to take revenge. And thats not a very calm and peaceful reaction. Remember, youre a priority.'
     },
     {
         "id": 4,
         "photo": '../../../assets/img/stock04.webp',
         "title": 'Growers having problem with dry weather',
-        "text": 'Drier weather has helped growers catch up with harvest, but quality is very variable, impacting on prices.'
+        "text": 'Theres a saying if you dont like the weather in Arkansas or (substitute your state here) just wait a little while, and it will change. The debate over how much the climate is changing has been raging for years, but theres little doubt Midsouth producers are dealing with more variable weather thats forcing them to consider adjustments in how they farm. Whether it’s unusually cold, wet springs, 2021’s foot-and-a-half of rain falling in Southeast Arkansas or 2022’s record heat and drought in June weather conditions have growers and agronomists shaking their heads.'
     },
 ]
