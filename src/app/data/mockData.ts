aeba22fdcb19271865b7cb226ad4f6afea9b47d1
Caio Batista <caio_b_s@hotmail.com>
2023-08-18 15:09:51 -0300

    feat: created data archive

---
+++ src/app/data/mockData.ts
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        "id": 1,
+        "photo": '../../../assets/img/stock01.jpg',
+        "title": 'Route 66: A legendary road',
+        "text": 'The road is full of things that hark back to the past too from old cars dotted along the road to old signs and historic buildings. For anyone who loves history or just wants a bit of an old fashioned vacation Route 66 fits the bill.'
+    },
+    {
+        "id": 2,
+        "photo": '../../../assets/img/stock02.jpg',
+        "title": 'Astronomic picture wins the Pulitzer',
+        "text": 'Picture from the Tulu Mountains with the Milky Way in the background wins Pulitzer.'
+    },
+    {
+        "id": 3,
+        "photo": '../../../assets/img/stock03.webp',
+        "title": 'Get rid of stress in your life',
+        "text": 'Is stress making you angry and grouchy? Stress relievers can help bring back calm and peace to your life.'
+    },
+    {
+        "id": 4,
+        "photo": '../../../assets/img/stock04.webp',
+        "title": 'Growers having problem with dry weather',
+        "text": 'Drier weather has helped growers catch up with harvest, but quality is very variable, impacting on prices.'
+    },
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+  cover: string = '../../../assets/img/stock01.jpg'
+  contentTitle: string = 'Meu Artigo'
+  contentText: string = 'A descrição do meu artigo.'
+
 
 }
